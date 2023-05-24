cfeb5e2e3f1fbd0ce089867d5429fe45d56436e9
Moffat Maina <elijah.michaelson.it@gmail.com>
2023-05-24 12:12:09 +0300

    Customize Temp

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 # Changelog
 
-All notable changes to `:package_name` will be documented in this file.
+All notable changes to `FastApi` will be documented in this file.
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace Tetra\FastApi\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
